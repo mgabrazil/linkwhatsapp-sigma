000c5fa304287ce8c95ac90ead9827ad803b3516
ATAÍDES <162513948+LeonardoAtaides@users.noreply.github.com>
2026-09-04 16:17:35 -0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "Objetivo · Unipaper",
+  name: "Sigma · Unipaper",
   description:
-    "Escolha a sua unidade e entre no grupo de WhatsApp do Objetivo para receber novidades e oportunidades exclusivas.",
+    "Escolha a sua unidade e entre no grupo de WhatsApp do Sigma",
   locale: "pt-BR",
 } as const;
